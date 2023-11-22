31fed83e7f0f1fb3b9799392364dd2fa34d75f99
nico-alv <nicolas.alvarez02@alumnos.ucn.cl>
2023-11-22 15:26:25 -0300

    feat: functional backend

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -18,34 +19,18 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
         Product::create($request->validated());
-        return response()->json("Producto añadido");
+        return response()->json("Producto añadido.");
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        return response()->json(Product::find($product->id));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        
-    }
-
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(StoreProductRequest $request, Product $product)
     {
-        //
+        $product->update($request->validated());
+        return $product;
     }
 
     /**
@@ -53,82 +38,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Application;
-use Illuminate\Http\Request;
-use App\Http\Requests\StoreApplicationRequest;
-
-class ApplicationController extends Controller
-{
-    public function index()
-    {
-        return response()->json(Application::all());
-    }
-
-    public function show($id)
-    {
-        return response()->json(Application::find($id));
-    }
-
-    public function store(StoreApplicationRequest $request){
-        Application::create($request->validated());
-        return response()->json("Usuario creado");
-    }
-
-    public function update(StoreApplicationRequest $request, Application $Application)
-    {
-        $Application->update($request->validated());
-        return $Application;
-    }
-
-    public function destroy(Application $Application){
-        $Application->delete();
-        return response()->json("Usuario eliminado");
+        $product->delete();
+        return response()->json("Producto eliminado.");
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table("products")->insert([
+            'name' => "Queso",
+            'image' => "test",
+            'price' => 1000,
+            'description' => "Producto lacteo"
+        ]);
     }
 }
